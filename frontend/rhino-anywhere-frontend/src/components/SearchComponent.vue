76e623e225f1856ababe23724c17ab7fdd44ccc8
just-ajs <35227625+just-ajs@users.noreply.github.com>
2023-11-04 14:58:32 -0400

    icon folder added

--- frontend/rhino-anywhere-frontend/src/components/SearchComponent.vue
+++ frontend/rhino-anywhere-frontend/src/components/SearchComponent.vue
@@ -1,24 +1,57 @@
 <script setup>
-import { ref } from 'vue';
-let searchInput = ref('')
+import { ref } from "vue"
+let search = ref("")
 let searchHistory = ref([])
+
+let commands = ["aa", "bb", "cc"]
+let filteredCommands = ref([])
+let highlightedIndex = ref(0)
 
 // Method to add the search term to the history
 const addSearchTerm = () => {
-    // Avoid adding empty strings or duplicates
-    if (searchInput.value && !searchHistory.value.includes(searchInput.value)) {
-    searchHistory.value.push(searchInput.value);
-    searchInput.value = ''; // Clear the input after adding to history
-    }
-};
+  // Avoid adding empty strings or duplicates
+  if (search.value && !searchHistory.value.includes(search.value)) {
+    searchHistory.value.push(search.value)
+    search.value = "" // Clear the input after adding to history
+  }
+}
+
+const filterCommands = () => {
+  if (!search) {
+    filteredCommands = []
+    return
+  }
+  filteredCommands = commands.filter((cmd) =>
+    cmd.toLowerCase().includes(search)
+  )
+}
+
+const highlightNext = () => {
+  if (highlightedIndex < filteredCommands.length - 1) {
+    highlightedIndex++
+  }
+}
+
+const highlightPrevious = () => {
+  if (highlightedIndex > 0) {
+    highlightedIndex--
+  }
+}
+
+const selectCommand = (command) => {
+  search = command
+  filteredCommands = []
+}
 </script>
 
 <template>
- <div class="search-container">
+  <div class="search-container">
     <!-- Display the search history -->
     <div class="search-history">
       <ul>
-        <li v-for="(entry, index) in searchHistory" :key="index">{{ entry }}</li>
+        <li v-for="(entry, index) in searchHistory" :key="index">
+          {{ entry }}
+        </li>
       </ul>
     </div>
 
@@ -26,9 +59,31 @@ const addSearchTerm = () => {
     <input
       type="text"
       placeholder="Type your Rhino command here..."
-      v-model="searchInput"
+      v-model="search"
       @keyup.enter="addSearchTerm"
     />
+
+    <!-- <div class="autocomplete">
+      <input
+        type="text"
+        v-model="search"
+        @input="filterCommands"
+        @keydown.down.prevent="highlightNext"
+        @keydown.up.prevent="highlightPrevious"
+        @keydown.enter.prevent="selectCommand"
+      />
+      <ul v-if="filteredCommands.length">
+        <li
+          v-for="(command, index) in filteredCommands"
+          :key="command"
+          :class="{ highlight: index === highlightedIndex }"
+          @mousedown="selectCommand(command)"
+        >
+          {{ command }}
+        </li>
+      </ul>
+    </div> -->
+
   </div>
 </template>
 
@@ -42,13 +97,25 @@ const addSearchTerm = () => {
 
 .search-history {
   margin-bottom: 10px;
+  height: 150px;
+  width: 600px;
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column-reverse;
+  border: 1px solid #ccc;
+  text-align: left;
+  border-radius: 20px;
 }
 
 input[type="text"] {
-  padding: 10px;
+  padding: 10px 15px;
   font-size: 16px;
-  width: 100%;
+  width: 600px;
   max-width: 400px; /* Adjust the width as needed */
+  border-radius: 20px;
+  border: 1px solid #ccc;
+  outline: none;
+  width: 500px;
 }
 
 ul.no-bullets {
@@ -59,7 +126,9 @@ ul.no-bullets {
 
 ul {
   list-style-type: none;
-  padding-left: 0;
+  padding-left: 10px;
   margin-left: 0;
+  line-height: 1;
+  color: grey;
 }
 </style>
