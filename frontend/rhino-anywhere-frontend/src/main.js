ccf02e839c3c754401cb03f888fbd9597228f20e
Josh Grift <josh@grift.ca>
2023-11-04 13:39:44 -0700

    Moved Anywhere

--- frontend/rhino-anywhere-frontend/src/main.js
+++ frontend/rhino-anywhere-frontend/src/main.js
@@ -3,6 +3,7 @@ import './style.css';
 import App from './App.vue';
 import { RhinoAnywhere } from './lib/anywhere';
 
+window.anywhere = new RhinoAnywhere();
+
 createApp(App).mount('#app');
 
-window.anywhere = new RhinoAnywhere();
