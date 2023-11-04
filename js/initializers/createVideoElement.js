7f95359de7a2439f33af7041c8fc4822acf02d1e
Kyle Slugg <kyle.slugg@gmail.com>
2023-11-04 14:05:25 -0400

    Laying out directory structure for initializers

---
+++ js/initializers/createVideoElement.js
@@ -0,0 +1,11 @@
+/**
+ * Video Element Creator
+ * @param {HTMLVideoElement} element
+ * @param {string} url
+ */
+export default createVideoElement = (element, url) => {
+  window.localConnection = localConnection = new RTCPeerConnection(
+    servers,
+    pcConstraint
+  );
+};
