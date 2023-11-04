86c938bf9cf90d525b150bb07b3d59d0a357ea6c
Josh Grift <josh@grift.ca>
2023-11-04 10:54:31 -0700

    Added comments

--- js/anywhere.js
+++ js/anywhere.js
@@ -4,8 +4,11 @@
  * @param {string} url
  */
 function anywhere(element, url) {
-  //setup
-  (() => {})();
+  // TODO: Setup event listeners
+
+  // TODO: Setup connection
+
+  // TODO: TODO create public methods to allow us to send command and mouse movements
 
   function _setup() {}
 
