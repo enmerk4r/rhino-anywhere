6bf600388748219f876a6ad5f461af96b17cadad
Josh Grift <josh@grift.ca>
2023-11-04 13:06:43 -0700

    Button

--- frontend/rhino-anywhere-frontend/src/components/VideoComponent.vue
+++ frontend/rhino-anywhere-frontend/src/components/VideoComponent.vue
@@ -6,10 +6,11 @@ function initialize() {
     anywhere(document.getElementById("rhinoViewport"), document.getElementById("rhinoTextBox"), "ws://127.0.0.1:8081/");
 }
 
-onMounted(() => initialize());
+//onMounted(() => initialize());
 </script>
 
 <template>
     <div id="rhinoTextBox"></div>
     <video controls autoplay="autoplay" id="rhinoViewport" width="640" height="480"></video>
+    <button @click="initialize">start</button>
 </template>
