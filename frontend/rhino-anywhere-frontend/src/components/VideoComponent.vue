754d893f69996f96ba64608473b238f9c600213c
Josh Grift <josh@grift.ca>
2023-11-04 15:36:11 -0700

    Change url

--- frontend/rhino-anywhere-frontend/src/components/VideoComponent.vue
+++ frontend/rhino-anywhere-frontend/src/components/VideoComponent.vue
@@ -3,14 +3,11 @@ import { onMounted } from 'vue';
 
 function initialize() {
     window.anywhere.bind(document.getElementById("rhinoViewport"));
-    window.anywhere.connect("ws://127.0.0.1:8081/");
 }
 
-//onMounted(() => initialize());
+onMounted(() => initialize());
 </script>
 
 <template>
-    <div id="rhinoTextBox"></div>
     <video controls autoplay="autoplay" id="rhinoViewport" width="640" height="480"></video>
-    <button @click="initialize">start</button>
 </template>
