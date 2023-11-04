89d06cffc1190eb9eb6d87bc2d7c6e53d3f9d233
enmerk4r <p.i.g.a.c.h@gmail.com>
2023-11-04 14:09:00 -0400

    Test commands

---
+++ RhinoAnywhere/RhinoAnywhere/FrameBufferTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Rhino;
+using Rhino.Commands;
+
+namespace RhinoAnywhere
+{
+    public class FrameBufferTest : Command
+    {
+        public FrameBufferTest()
+        {
+            Instance = this;
+        }
+
+        ///<summary>The only instance of the MyCommand command.</summary>
+        public static FrameBufferTest Instance { get; private set; }
+
+        public override string EnglishName => "FrameBufferTest";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            // TODO: complete command.
+            return Result.Success;
+        }
+    }
+}
