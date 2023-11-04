1fb6eb849f5272e46adb0eda5c4717af2ae54b5d
clicketyclackety <csykes@duck.com>
2023-11-04 15:27:58 -0700

    input

---
+++ RhinoAnywhere/RhinoAnywhere/Input.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RhinoAnywhereCore
+{
+    public class InputEventArgs : EventArgs
+    {
+        public string Type { get; set; }
+        public InputData Data { get; set; }
+    }
+
+    public class InputData
+    {
+        public string Method { get; set; }
+        public string Action { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int DeltaX { get; set; }
+        public int DeltaY { get; set; }
+        public string Value { get; set; }
+    }
+}
