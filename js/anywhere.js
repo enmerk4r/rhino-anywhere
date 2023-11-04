7f7e6e0352f8fd5e07831c42453be0ccf5a4181f
Josh Grift <josh@grift.ca>
2023-11-04 10:46:35 -0700

    Boilerplate library

---
+++ js/anywhere.js
@@ -0,0 +1,13 @@
+/**
+ * Anywhere Library Creator
+ * @param {HTMLVideoElement} element
+ * @param {string} url
+ */
+function anywhere(element, url) {
+  //setup
+  (() => {})();
+
+  function _setup() {}
+
+  function sendCommand() {}
+}
