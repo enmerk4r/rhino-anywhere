98f2a630909a192a96cee01f88306dadc6d5805c
Kyle Slugg <kyle.slugg@gmail.com>
2023-11-04 14:54:20 -0400

    Tentative method for establishing local connection

--- frontend/rhino-anywhere-frontend/src/lib/anywhere.js
+++ frontend/rhino-anywhere-frontend/src/lib/anywhere.js
@@ -1,17 +1,23 @@
+import initializeLocalConnection from './initializers/initializeLocalConnection';
+
 /**
  * Anywhere Library Creator
  * @param {HTMLVideoElement} element
  * @param {string} url
  */
 export function anywhere(element, url) {
-  console.log("Setting up RhinoAnywhere");
+  console.log('Setting up RhinoAnywhere');
   // TODO: Setup event listeners
 
   // TODO: Setup connection
 
   // TODO: TODO create public methods to allow us to send command and mouse movements
+  let signalChannel;
 
-  function _setup() {}
+  function _setup() {
+    signalChannel = new WebSocket(url, []);
+    initializeLocalConnection(signalChannel, element); //Need to establish vars for data input and output
+  }
 
   function sendCommand() {}
 }
