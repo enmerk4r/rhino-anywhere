55d68c6a1bdb6c29a4c5e0594dce11fa91346c3e
just-ajs <35227625+just-ajs@users.noreply.github.com>
2023-11-04 13:56:03 -0400

    rhino title

--- frontend/rhino-anywhere-frontend/src/App.vue
+++ frontend/rhino-anywhere-frontend/src/App.vue
@@ -1,17 +1,12 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Rhino anywhere" />
+    <img src="./assets/img/rhino.png" alt="some nice logo"  style="width: 100px; height: 100px;"/>
+  <h1>Rhino anywhere</h1>
+</div>  
 </template>
 
 <style scoped>
