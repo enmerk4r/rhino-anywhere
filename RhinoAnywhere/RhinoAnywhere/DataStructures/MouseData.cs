b7f47bd699dfe347c9649da90343d9576b93a876
clicketyclackety <csykes@duck.com>
2023-11-04 18:48:10 -0700

    Moved Data Structures out

---
+++ RhinoAnywhere/RhinoAnywhere/DataStructures/MouseData.cs
@@ -0,0 +1,18 @@
+﻿namespace RhinoAnywhere
+{
+
+
+    public sealed partial class StartRhinoAnywhere
+    {
+        public struct MouseData
+        {
+            public string method { get; set; }
+            public string action { get; set; }
+            public double x { get; set; }
+            public double y { get; set; }
+            public double deltax { get; set; }
+            public double deltay { get; set; }
+            public string value { get; set; }
+        }
+    }
+}
