3f549c3da34e295cfefece4a95d16975d0c5c131
enmerk4r <p.i.g.a.c.h@gmail.com>
2023-11-04 23:00:08 -0400

    Cleanup

--- RhinoAnywhere/RhinoAnywhere/StartRhinoAnywhere.cs
+++ RhinoAnywhere/RhinoAnywhere/StartRhinoAnywhere.cs
@@ -68,6 +68,7 @@ namespace RhinoAnywhere
         private static void RegisterPipelineCall()
         {
             DisplayPipeline.DrawOverlay += DisplayPipeline_PostDrawObjects;
+
         }
 
         private static DateTime LastCall { get; set; } = DateTime.UtcNow;
@@ -83,14 +84,8 @@ namespace RhinoAnywhere
             if (SocketServer is null)
                 return;
 
-            RhinoView activeView = RhinoDoc.ActiveDoc.Views.ActiveView;
-
-            var size = activeView.Size;
-            using (var bitmap = new Bitmap(size.Width, size.Height))
-            {
-                Bitmap LastBitMap = e.Display.FrameBuffer;
-                SendBitmap(LastBitMap);
-            }
+            Bitmap LastBitMap = e.Display.FrameBuffer;
+            SendBitmap(LastBitMap);
         }
 
 
@@ -188,24 +183,6 @@ namespace RhinoAnywhere
             byte[] rgbValues = new byte[bytes];
 
             Marshal.Copy(ptr, rgbValues, 0, bytes);
-
-            // BRGA => RGBA conversion
-            for (int i = 0; i < bitmap.Height; i++)
-            {
-                for (int j = 0; j < bitmap.Width; j++)
-                {
-                    var idx = (i * bitmap.Width + j) * 4;
-                    var b = rgbValues[idx + 0];
-                    var g = rgbValues[idx + 1];
-                    var r = rgbValues[idx + 2];
-                    var a = rgbValues[idx + 3];
-
-                    rgbValues[idx + 0] = r;
-                    rgbValues[idx + 1] = g;
-                    rgbValues[idx + 2] = b;
-                    rgbValues[idx + 3] = a;
-                }
-            }
 
             Connection.SendVideo(DurationUnits, Encoder.EncodeVideo(bitmap.Width, bitmap.Height, rgbValues, VideoPixelFormatsEnum.Bgra, VideoCodecsEnum.H265));
         }
